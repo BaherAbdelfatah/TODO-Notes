917da6a3f0efde1d2814d1bc4a09ffa70acbd4fb
BaherAbdelfatah <baherbeho_ls@yahoo.com>
2020-05-19 21:19:27 +0200

    Testing Coroutine Timing

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
@@ -0,0 +1,57 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.core.Is.`is`
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class StatisticsViewModelTest {
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    // Subject under test
+    private lateinit var statisticsViewModel: StatisticsViewModel
+
+    // Use a fake repository to be injected into the view model.
+    private lateinit var tasksRepository: FakeTestRepository
+
+    @Before
+    fun setupStatisticsViewModel() {
+        // Initialise the repository with no tasks.
+        tasksRepository = FakeTestRepository()
+
+        statisticsViewModel = StatisticsViewModel(tasksRepository)
+    }
+
+    @Test
+    fun loadTasks_loading() {
+        // Pause dispatcher so you can verify initial values.
+        mainCoroutineRule.pauseDispatcher()
+
+        // Load the task in the view model.
+        statisticsViewModel.refresh()
+
+        // Then assert that the progress indicator is shown.
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(true))
+
+        // Execute pending coroutines actions.
+        mainCoroutineRule.resumeDispatcher()
+
+        // Then assert that the progress indicator is hidden.
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(false))
+    }
+}
